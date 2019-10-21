fdfe2c3877a0ad2df080bd17756c14ad4e828073
sagudeloo <sagudeloo@eafit.edu.co>
2019-10-20 21:57:31 -0500

    server y pagina funcionando

--- sirius_server/src/public/main.js
+++ sirius_server/src/public/main.js
@@ -1,9 +1,41 @@
 const socket = io();
+const socket2 = io();
 
-socket.on('serial', function(data){
-    document.body.innerHTML = '';
-    document.write(JSON.stringify({"posicion": data[0],
-    "iman": data[1],
-    "tamanno_disc": data[2]
-    }));
+let posicion = document.getElementById('posicion');
+let iman = document.getElementById('iman');
+let tamanno_disc = document.getElementById('tamanno_disc');
+
+socket.on('read_from_arduino', (data) => {
+  if (data['iman'] == 1) {
+    iman.className = 'btn btn-success rounded-pill btn-lg';
+  }else{
+    iman.className = 'btn btn-danger rounded-pill btn-lg';
+  }
+  np = data['posicion']
+  posicion.innerHTML = '<h4>Posicion</h4><br> <div class="row d-flex align-items-center justify-content-around"><div class="'+ (np == 1? 'ps':'p') + '"></div><div class="'+ (np == 2? 'ps':'p') +'"></div><div class="'+ (np == 3? 'ps':'p') +'"></div></div>'
+  tamanno_disc.innerHTML = '<h4>Disco</h4><br> <div class="d-flex align-items-center justify-content-around"><div class="disco-' + data['tamanno_disc'] + '"></div></div>'
 });
+
+function up(){
+  send_data('u');
+}
+
+function down(){
+  send_data('d');
+}
+
+function left(){
+  send_data('l');
+}
+
+function right(){
+  send_data('r');
+}
+
+function magnet(){
+  send_data('e');
+}
+
+function send_data(data){
+  socket.emit('send_to_arduino', data);
+}
